dd5c573ecf298c5e9bcf12bfede509df1688f6e4
JosDeleon <jose.deleon@galileo.edu>
2023-12-04 21:42:54 -0600

    Cambios en despliegue

--- config/apollo.js
+++ config/apollo.js
@@ -2,8 +2,12 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 import { onError } from "@apollo/client/link/error";
 
+// const httpLink = createHttpLink({
+//     uri : 'https://morning-journey-09515.herokuapp.com/',
+//     fetch
+// });
 const httpLink = createHttpLink({
-    uri : 'https://morning-journey-09515.herokuapp.com/',
+    uri : 'https://656e9a254c37ab0497a9b327--rococo-hotteok-094ba8.netlify.app/',
     fetch
 });
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
 
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
 import { ApolloProvider } from '@apollo/client';
 import client from '../config/apollo';
 import PedidoState from '../context/pedidos/PedidoState';
